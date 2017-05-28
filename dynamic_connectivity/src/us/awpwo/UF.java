7adba3e14c0e04ad15c588bf9baee980c8ce6852
Your Name <you@example.com>
2017-05-28 10:03:48 +0200

    method union modified

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -15,8 +15,15 @@ public boolean connected(int p, int q) {
 }
 public void union(int p, int q) {
  int i = root(p);
- int j = root(q);
+int j = root(q);
+if (i == j) return;
+ if (sz[i] < sz[j]) {
  id[i] = j;
+ sz[j] += sz[i];
+ }
+else {
+ id[j] = i;
+ sz[i] += sz[j];
 }
 	
 }
